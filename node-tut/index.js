1e21d4d0c3afe0731b12f0416a9c272b44728886
KateZaharova <124520175+KateZaharova@users.noreply.github.com>
2026-03-26 18:41:15 +0200

    node.js

--- node-tut/index.js
+++ node-tut/index.js
@@ -1,2 +1,62 @@
-const message = "Hello, Node.js!";
-console.log(message);
+/*const object = {
+    "name": "Josh",
+    "weight": 175,
+    "age": 30,
+    "eyecolor": "brown",
+    "isHappy": true,
+    "cars": ["Chevy", "Honda"],
+    "favoriteBook": {
+        "title": "The Last Kingdom",
+        "author": "Bernard Cornwell",
+        "rating": 8.38
+    }
+};
+
+const objectJSON = JSON.stringify(object);
+console.log(objectJSON);
+
+const dog = {
+  name: "Mango",
+  age: 3,
+  isHappy: true,
+};
+
+const dogJSON = JSON.stringify(dog);
+console.log(dogJSON);
+
+const json = '{"name":"Mango","age":3,"isHappy":true}';
+
+const jsonParsed = JSON.parse(json);
+console.log(jsonParsed);
+console.log(jsonParsed.name) 
+
+try {
+  const data = JSON.parse("Well, this is awkward");
+} catch (error) {
+  console.log(error.name);// "SyntaxError"
+  console.log(error.message);// Unexpected token W in JSON at position 0
+};
+
+try {
+  const data = JSON.parse('{username: "Mango"}');
+} catch (error) {
+  console.log(error.name);// "SyntaxError"
+  console.log(error.message);// "Unexpected token u in JSON at position 1"
+}
+
+const settings = {
+  theme: "dark",
+  isAuthenticated: true,
+  options: [1, 2, 3],
+};
+
+localStorage.setItem("settings", JSON.stringify(settings));
+
+const savedSettings = localStorage.getItem("settings");
+console.log(savedSettings);
+const parsedSettings = JSON.parse(savedSettings);
+console.log(parsedSettings);// settings object
+*/
+
+console.log("Hi from Node.js");
+
